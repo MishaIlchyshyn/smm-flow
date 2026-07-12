6c5e28ad9a006bc2c7ab216cfc526f492b804e80
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2026-07-12 15:35:20 +0300

    Use turbo frames for projects inside clients

--- app/controllers/agencies/clients/projects_controller.rb
+++ app/controllers/agencies/clients/projects_controller.rb
@@ -6,19 +6,22 @@ module Agencies
 
       def new
         @project = @client.projects.build
+        redirect_to client_path(@client, tab: "projects") unless turbo_frame_request?
       end
 
       def create
         @project = @client.projects.build(project_params.merge(agency: current_agency))
 
         if @project.save
-          redirect_to client_project_path(@client, @project), notice: "Project created."
+          success_respond("Successfully saved!", "Project has been created.", client_project_path(@client, @project))
         else
           failed_respond("Couldn't save", errors_msg(@project), new_client_project_path(@client))
         end
       end
 
-      def show; end
+      def show
+        redirect_to client_path(@client, tab: "projects") unless turbo_frame_request?
+      end
 
       def update
         if @project.update(project_params)
